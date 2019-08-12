32832708e5e9543f6d6bc8a3b2b51d560c3e4048
piaoliangkb <418508556@qq.com>
2019-08-12 23:55:06 +0800

    upload exercises 7.1 7.2 7.3

---
+++ chapter7/7.2_7.3.cpp
@@ -0,0 +1,29 @@
+#include "Sales_data.h"
+
+int main()
+{
+    Sales_data total;
+    if (std::cin >> total.bookNo >> total.units_sold >> total.revenue)
+    {
+        Sales_data trans;
+        while (std::cin >> trans.bookNo >> trans.units_sold >> trans.revenue)
+        {
+            if (total.isbn() == trans.isbn())
+            {
+                total.combine(trans);
+            }
+            else
+            {
+                total.Print();
+                total = trans;
+            }
+        }
+        total.Print();
+    }
+    else
+    {
+        std::cout << "No data input." << std::endl;
+    }
+
+    return 0;
+}
---
+++ chapter7/Sales_data.h
@@ -0,0 +1,43 @@
+#ifndef SALES_DATA_H
+#define SALES_DATA_H
+
+#include <iostream>
+#include <string>
+
+struct Sales_data {
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+
+    void Adddata(Sales_data data);
+    void Print();
+
+    // exercises 7.2 
+    std::string isbn() const { return bookNo; }
+    Sales_data& combine(const Sales_data &data);
+};
+
+void Sales_data::Adddata(Sales_data data)
+{
+    if (bookNo != data.bookNo) return ;
+    units_sold += data.units_sold;
+    revenue += data.revenue;
+}
+
+void Sales_data::Print()
+{
+    std::cout << "{" << std::endl;
+    std::cout << "This book number is : " << bookNo << std::endl;
+    std::cout << "This book has been sold " << units_sold << std::endl;
+    std::cout << "All revenue is " << revenue << std::endl << "}" << std::endl;
+}
+
+// exercises 7.2
+Sales_data& Sales_data::combine(const Sales_data &data)
+{
+    units_sold += data.units_sold;
+    revenue += data.revenue;
+    return *this;
+}
+
+#endif
